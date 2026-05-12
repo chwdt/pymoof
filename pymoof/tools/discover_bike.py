209bbd11270181fc2b4967475e5675d7f59a7a21
Christian Wendt <cw@brainaid.de>
2026-05-12 09:17:16 +0200

    Add SX4 service uuid to discovery

--- pymoof/tools/discover_bike.py
+++ pymoof/tools/discover_bike.py
@@ -9,9 +9,13 @@ async def query():
             "6acc5540-e631-4069-944d-b8ca7598ad50",
             "8e7f1a50-087a-44c9-b292-a2c628fdd9aa",
             "6acb5520-e631-4069-944d-b8ca7598ad50",
+            "278d5540-4692-039f-3445-a23fc55333d0",
         ],
     )
     for device in devices:
+        if "278d5540-4692-039f-3445-a23fc55333d0" in device.metadata["uuids"]:
+            print("Found SX4:", device.address)
+            return device
         if "6acc5540-e631-4069-944d-b8ca7598ad50" in device.metadata["uuids"]:
             print("Found SX3:", device.address)
             return device
